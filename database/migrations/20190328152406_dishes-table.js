72069f411099eb3408ce7838db53a4080c15a9ff
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-28 16:12:04 +0000

    Add Migration Schema Files

--- database/migrations/20190328152406_dishes-table.js
+++ database/migrations/20190328152406_dishes-table.js
@@ -1,7 +1,14 @@
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('dishes', function(tableColumn) {
+    tableColumn.increments('dish_id');
 
+    tableColumn
+      .string('dish_name', 512)
+      .unique("uq_dish_name")
+      .notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTableIfExists('dishes');
 };
--- database/migrations/20190328152451_recipe-ingredients-table.js
+++ database/migrations/20190328152451_recipe-ingredients-table.js
@@ -1,7 +1,27 @@
 exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipe_ingredients', function(tableColumn) {
+    tableColumn.increments('recipe_ingredients_id');
+    tableColumn
+      .integer('recipe_id')
+      .references('recipe_id')
+      .inTable('recipes')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
 
+    tableColumn
+      .integer('ingredient_id')
+      .references('ingredient_id')
+      .inTable('ingredients')
+      .notNullable()
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+
+    tableColumn
+      .float('quantity');
+  })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTableIfExists('recipe_ingredients');
 };
