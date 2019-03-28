7ffa54f08774f1f1a35fc6b4de6587a3b96447c7
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-28 17:43:16 +0000

    Add Seed Files

--- database/seeds/02-recipes-seed.js
+++ database/seeds/02-recipes-seed.js
@@ -1,13 +1,52 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipes').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('recipes').insert([
+        {
+          recipe_id: 1,
+          dish_id: 1,
+          recipe_name: 'Red Velvet Cake',
+          instructions: "..."
+        },
+        {
+          recipe_id: 2,
+          dish_id: 1,
+          recipe_name: 'Chocolate Sprinkles Cake',
+          instructions: "..."
+        },
+        {
+          recipe_id: 3,
+          dish_id: 3,
+          recipe_name: 'Mozzarella Basil Pasta',
+          instructions: "..."
+        },
+        {
+          recipe_id: 4,
+          dish_id: 3,
+          recipe_name: 'BBQ Chicken Pasta',
+          instructions: "..."
+        },
+        {
+          recipe_id: 5,
+          dish_id: 2,
+          recipe_name: 'Potato Salad',
+          instructions: "..."
+        },
+        {
+          recipe_id: 6,
+          dish_id: 2,
+          recipe_name: 'Spicy Carrot Salad',
+          instructions: "..."
+        },
+        {
+          recipe_id: 7,
+          dish_id: 2,
+          recipe_name: 'Vietnamese Chicken Salad',
+          instructions: "..."
+        },
       ]);
     });
 };
--- database/seeds/04-ingredients-seed.js
+++ database/seeds/04-ingredients-seed.js
@@ -1,13 +1,54 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('ingredients').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('ingredients').insert([
+        {
+          ingredient_id: 1,
+          ingredient_name: "Chicken Breast"
+        },
+        {
+          ingredient_id: 2,
+          ingredient_name: "BBQ Sauce"
+        },
+        {
+          ingredient_id: 3,
+          ingredient_name: "Eggs"
+        },
+        {
+          ingredient_id: 4,
+          ingredient_name: "Chocolate Sprinkles"
+        },
+        {
+          ingredient_id: 5,
+          ingredient_name: "Flour"
+        },
+        {
+          ingredient_id: 6,
+          ingredient_name: "Tagliatelle"
+        },
+        {
+          ingredient_id: 7,
+          ingredient_name: "Organic Carrot"
+        },
+        {
+          ingredient_id: 8,
+          ingredient_name: "Organic Potato"
+        },
+        {
+          ingredient_id: 9,
+          ingredient_name: "Organic Lettuce"
+        },
+        {
+          ingredient_id: 10,
+          ingredient_name: "Mozzarella Cheese"
+        },
+        {
+          ingredient_id: 11,
+          ingredient_name: "Basil"
+        }
       ]);
     });
 };
